f1b80ce7d0f21a559f1d5f8e1f959e468e439987
TJW-0 <61374195+TJW-0@users.noreply.github.com>
2020-06-18 16:27:39 +0800

    Update FormMain.cs
    修改主菜单方法

--- FormMain.cs
+++ FormMain.cs
@@ -32,36 +32,11 @@ namespace Library
             }
             this.manageButton.Visible = false;
             this.manageButton.Enabled = false;
-            if(BookShelfService.AllBooks().Count == 0)
+            if(BookShelfService.AllBooks().Count == 0)//设置初始值
             {
-                BookShelf test1 = new BookShelf();
-                List<Book> Books1 = new List<Book>() {
-                    new Book() { BookId = "1", Name = "毛泽东选集", BookShelfId = "1",
-                    State = "可正常使用", Description = "毛泽东所写的一本书。", Recommend = 10, Author = "毛泽东", AppointedTime = null, ClientName = null, Sort = "文学" },
-                    new Book() { BookId = "2", Name = "毛泽东选集二", BookShelfId = "1", State = "已被借阅", Description = "毛泽东所写的一本书。", Recommend = 15, Author = "毛泽东", AppointedTime = "2020年8月1日", ClientName = "管理员", Sort = "文学" },
-                    new Book() { BookId = "3", Name = "毛泽东选集三", BookShelfId = "1", State = "已被借阅", Description = "毛泽东所写的一本书。", Recommend = 15, Author = "毛泽东", AppointedTime = "2020年8月1日", ClientName = "管理员", Sort = "文学" },
-                    new Book() { BookId = "4", Name = "毛泽东选集四", BookShelfId = "1", State = "已被预约", Description = "毛泽东所写的一本书。", Recommend = 13, Author = "毛泽东", AppointedTime = "2020年8月1日", ClientName = "管理员", Sort = "文学" }
-                };
-                foreach (var book in Books1)
-                {
-                    test1.AddBook(book);
-                }
-                test1.BookShelfId = "1";
-                BookShelfService.AddBookShelf(test1);
-                BookShelf test2 = new BookShelf();
-                List<Book> Books2 = new List<Book>() {
-                    new Book() { BookId = "5", Name = "C#编程", BookShelfId = "2",
-                    State = "可正常使用", Description = "C#编程指南。", Recommend = 14, Author = "佚名", AppointedTime = null, ClientName = null, Sort = "C#" },
-                    new Book() { BookId = "6", Name = "JAVA编程指南", BookShelfId = "2", State = "已被借阅", Description = "JAVA编程指南。", Recommend = 12, Author = "佚名", AppointedTime = "2020年8月1日", ClientName = "Admin", Sort = "JAVA" },
-                    new Book() { BookId = "7", Name = "C++编程指南", BookShelfId = "2", State = "已被预约", Description = "C++编程指南。", Recommend = 13, Author = "佚名", AppointedTime = "2020年8月1日", ClientName = "Admin" , Sort = "C++"}
-                };
-                foreach (var book in Books2)
-                {
-                    test2.AddBook(book);
-                }
-                test2.BookShelfId = "2";
-                BookShelfService.AddBookShelf(test2);
-            }
+                Test.Test1();
+                Test.Test2();
+            }      
 
             
             Books = BookShelfService.AllBooks();
