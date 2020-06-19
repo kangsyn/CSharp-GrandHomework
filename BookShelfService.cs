0b2203ebbbb70f5c4de59692fb8a1a17b0422988
TJW-0 <61374195+TJW-0@users.noreply.github.com>
2020-06-19 12:15:40 +0800

    Update BookShelfService.cs
    由于预约逻辑的修改和当前日期的引用，更改许多方法

--- BookShelfService.cs
+++ BookShelfService.cs
@@ -76,7 +76,7 @@ namespace Library
             }
         }
 
-        public static void DeleteBookShelf(string id)
+        public static void DeleteBookShelf(string id)//删除书架
         {
             try
             {
@@ -157,7 +157,7 @@ namespace Library
         public static void UpdateBook(Book newBook)//更新数据库内的书籍
         {
             RemoveBooks(newBook.BookId);
-            MessageBox.Show(BookShelfService.AllBooks().Count + "" + "3");
+            //MessageBox.Show(BookShelfService.AllBooks().Count + "" + "3");
             using (var db = new BookShelfContext())
             {
                 db.Entry(newBook).State = EntityState.Modified;
@@ -177,13 +177,13 @@ namespace Library
                 book.ClientName = client.Name;
             }
             book.State = state;
-            MessageBox.Show(BookShelfService.AllBooks().Count + "" + "1");
+            //MessageBox.Show(BookShelfService.AllBooks().Count + "" + "1");
             using (var db = new BookShelfContext())
             {
                 BookShelf bookShelf = AllShelves(db).FirstOrDefault(o => o.BookShelfId == book.BookShelfId);
                 UpdateBook(book);
             }
-            MessageBox.Show(BookShelfService.AllBooks().Count + "" + "4");
+            //MessageBox.Show(BookShelfService.AllBooks().Count + "" + "4");
         }
 
         public static List<Book> GetAllLentBooks(Client client)//取得所有已借阅的书
@@ -196,28 +196,89 @@ namespace Library
 
         public static List<Book> GetAllAppointedBooks(Client client)//取得所有预约的书
         {
+            //MessageBox.Show(client.Name);
             using (var db = new BookShelfContext())
             {
-                return db.Books.Where(o => o.State == "已被预约").Where(p => p.ClientName == client.Name).ToList();
+                List<Book> appointed = new List<Book>();
+                foreach(var b in db.Books.Where(o => o.State == "已被借阅").ToList())
+                {
+                    if (b.Appointers.Contains(client.Name))
+                    {
+                                appointed.Add(b);
+                    }
+                }
+                //MessageBox.Show(appointed.Count()+"");
+                return appointed;
             }
         }
 
-        public static void ReNewLending(Book book, string newDate)//续借新书
+        public static void ReNewLending(Book book, Client client, string year,string month,string day)//续借新书
         {
-            book.AppointedTime = time;
-            //book.Appointers.Add(client);
-            SetBookState(book, "已被借阅", client);
+            int Year = DateTime.Now.Year;
+            int Month = DateTime.Now.Month;
+            int Day = DateTime.Now.Day;
+            int Days = DateTime.DaysInMonth(Year, Month);
+            //MessageBox.Show(Year + " " + Month + " " + Day + " " + "|" + year + " " + month + " " + day + " "+Days);
+            bool flag = true;
+            if (Convert.ToInt32(year) < Year)
+            {
+                flag = false;
+            }
+            else if(Convert.ToInt32(year) == Year)
+            {
+                if (Convert.ToInt32(month) < Month)
+                {
+                    flag = false;
+                }
+                else if (Convert.ToInt32(month) == Month)
+                {
+                    if (Convert.ToInt32(day) <= Day)
+                    {
+                        flag = false;
+                    }
+                }
+            }
+            if (Convert.ToInt32(day) > Days)
+            {
+                flag = false;
+            }
+            if(flag == true)
+            {
+                book.LendTime = year + "年" + month + "月" + day + "日";
+                //book.Appointers.Add(client);
+                SetBookState(book, "已被借阅", client);
+            }
+            if(flag == false)
+            {
+                MessageBox.Show("没有输入正确的日期！");
+            }
         }
         
         public static void ReturnBooks(Book book, Client client)//还书
         {
-            SetBookState(book, "可正常使用", client);
+            if(book.Appointers == "" || book.Appointers == null)
+            {
+                SetBookState(book, "可正常使用", client);
+            }
+            else
+            {
+                int i = book.Appointers.IndexOf(" ");
+                string c = book.Appointers.Substring(0, i);
+                //MessageBox.Show(c);
+                book.Appointers = book.Appointers.Replace(c + " ", "");
+                Client newClient = ClientService.AllClients().FirstOrDefault(o => o.Name == c);
+                int Year = DateTime.Now.Year;
+                int Month = DateTime.Now.Month+1;
+                int Day = DateTime.Now.Day;
+                book.LendTime = Year + "年" + Month + "月" + Day + "日";
+                SetBookState(book, "已被借阅", newClient);
+            }
         }
 
         public static void CancellAppoint(Book book, Client client)//取消预约
         {
-            book.AppointedTime = null;
-            SetBookState(book, "可正常使用", client);
+            book.Appointers = book.Appointers.Replace(client.Name+" ", "");
+            UpdateBook(book);
         }
 
         public static void AppointBook(Book book, Client client, string time)//预约
@@ -230,6 +291,43 @@ namespace Library
         public static void LendBook(Book book, Client client)//借阅
         {
             SetBookState(book, "可正常使用", client);
+        }
+        
+        public static void Check(int year,int month, int day)//检测是否有书籍到期
+        {
+            using (var db = new BookShelfContext())
+            {
+                List<Book> allLentBooks = db.Books.Where(o => o.State == "已被借阅").ToList();
+                foreach(var b in allLentBooks)
+                {
+                    String time = b.LendTime;
+                    int y = time.IndexOf("年");
+                    int m = time.IndexOf("月");
+                    int d = time.IndexOf("日");
+                    String bookYear = time.Substring(0, y);
+                    String bookMonth = time.Substring(y + 1, m - y - 1);
+                    String bookDay = time.Substring(m + 1, d - m - 1);
+                    Client user = ClientService.AllClients().FirstOrDefault(o => o.Name == b.ClientName);
+                    if (Convert.ToInt32(bookYear) == year)
+                    {
+                        if(Convert.ToInt32(bookMonth) == month)
+                        {
+                            if(Convert.ToInt32(bookDay) <= day)
+                            {
+                                ReturnBooks(b, user);
+                            }
+                        }
+                        else if(Convert.ToInt32(bookMonth) < month)
+                        {
+                            ReturnBooks(b, user);
+                        }
+                    }
+                    else if(Convert.ToInt32(bookYear) < year)
+                    {
+                        ReturnBooks(b, user);
+                    }
+                }
+            }
         }
     }
 }
