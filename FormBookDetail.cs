53b84a4c6cfdb5ba40a03f8de85eabdc73602ad7
TJW-0 <61374195+TJW-0@users.noreply.github.com>
2020-06-19 22:44:18 +0800

    Update FormBookDetail.cs
    刚才的更新忘记更改具体内容了。。。现在补上。

--- FormBookDetail.cs
+++ FormBookDetail.cs
@@ -13,18 +13,25 @@ namespace Library
 {
     public partial class FormBookDetail : Form
     {
+        public BookShelf currentShelf { get; set; }
         public Client currentClient { get; set; }//现在正在使用的用户
         public Book currentBook { get; set; }//现在正在查看的图书
-        public int Flag = 0;//指示该界面由主界面还是图书管理界面进入（可能无用）
-        public FormBookDetail()//无参数，表示是由添加新书按钮进入，所有文本框均为空并可用，但借阅、预约、推荐相关组件不可使用，基本完成
+        public string path { get; set; }
+        public static string resPath { get; set; }
+        public int Flag = 0;//指示该界面由主界面还是图书管理界面进入
+        public FormBookDetail()//无参数，表示是由添加新书按钮进入，所有文本框均为空并可用，但借阅、预约、推荐相关组件不可使用完成
         {
             InitializeComponent();
+            currentShelf = shelf;
             this.lendButton.Enabled = false;
             this.lendButton.Visible = false;
             this.recommendButton.Enabled = false;
             this.recommendButton.Visible = false;
             this.appointButton.Enabled = false;
             this.appointButton.Visible = false;
+            this.shelfTextBox.Text = shelf.BookShelfId;
+            this.shelfTextBox.ReadOnly = true;
+
             //与预约相关的控件
             this.textBox1.Enabled = false;
             this.textBox1.Visible = false;
@@ -38,9 +45,10 @@ namespace Library
             this.monthComboBox.Visible = false;
             this.dayComboBox.Enabled = false;
             this.dayComboBox.Visible = false;
+            findPath();
         }
 
-        public FormBookDetail(Book book,Client client, int flag)//表示是由查看书籍详情进入的,flag的值取1、2（分别表示从主界面进入和从图书管理界面进入，前者只读），基本完成
+        public FormBookDetail(Book book,Client client, int flag)//表示是由查看书籍详情进入的,flag的值取1、2（分别表示从主界面进入和从图书管理界面进入，前者只读），完成
         {
             InitializeComponent();
             this.nameTextBox.Text = book.Name;
@@ -48,8 +56,15 @@ namespace Library
             this.sortTextBox.Text = book.Sort;
             this.shelfTextBox.Text = book.BookShelfId;
             this.introductionTextBox.Text = book.Description;
+            this.currentClient = client;
+            this.currentBook = book;
+            if(book.imagePath != null)
+            {
+                this.discoverPictureBox.Image = Image.FromFile(book.imagePath);
+            }
             if (flag == 1)//从主界面进入则不能更改内容，加入书架按钮也被隐藏
             {
+                Flag = 1;
                 this.nameTextBox.ReadOnly = true;
                 this.authorTextBox.ReadOnly = true;
                 this.sortTextBox.ReadOnly = true;
@@ -60,6 +75,8 @@ namespace Library
             }
             else//从图书管理界面进入则借阅、推荐、预约按钮被隐藏
             {
+                Flag = 2;
+                this.shelfTextBox.ReadOnly = true;
                 this.lendButton.Enabled = false;
                 this.lendButton.Visible = false;
                 this.recommendButton.Enabled = false;
@@ -79,32 +96,107 @@ namespace Library
                 this.monthComboBox.Visible = false;
                 this.dayComboBox.Enabled = false;
                 this.dayComboBox.Visible = false;
+                this.addButton.Enabled = false;
+                this.addButton.Visible = false;
             }
+            findPath();
         }
 
         private void lendButton_Click(object sender, EventArgs e)//借阅,当图书可用时将图书的状态改为借阅
         {
-
+            if (yearComboBox.Text != "" && monthComboBox.Text != "" && dayComboBox.Text != "")
+            {
+                string year = yearComboBox.Text;
+                string month = monthComboBox.Text;
+                string day = dayComboBox.Text;
+                BookShelfService.LendBook(currentBook, currentClient, year, month, day);
+            }
+            else
+            {
+                MessageBox.Show("没有输入正确的日期！", "系统提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
 
         private void recommendButton_Click(object sender, EventArgs e)//推荐，推荐数+1
         {
-
+            currentBook.Recommend = currentBook.Recommend + 1;
+            BookShelfService.UpdateBook(currentBook);
+            this.recommendButton.Enabled = false;
+            this.recommendButton.Visible = false;
         }
 
-        private void appointButton_Click(object sender, EventArgs e)//预约，预约时间必须后于当前时间（可能要涉及DateTime），并同时改动预约情况
+        private void appointButton_Click(object sender, EventArgs e)//预约
         {
-
+            BookShelfService.AppointBook(currentBook, currentClient);
+            this.appointButton.Enabled = false;
+            this.appointButton.Visible = false;
         }
 
         private void addButton_Click(object sender, EventArgs e)//增加图书
         {
-
+            if (nameTextBox.Text != null && authorTextBox.Text != null && sortTextBox.Text != null && introductionTextBox.Text != null)
+            {
+                int k = Convert.ToInt32(BookShelfService.AllBooks().Max(i => i.BookId)) + 1;
+                string bookId = k + "";
+                string name = this.nameTextBox.Text;
+                string author = this.authorTextBox.Text;
+                string bookShelfId = currentShelf.BookShelfId;
+                string lendTime = null;
+                string clientName = null;
+                string sort = this.sortTextBox.Text;
+                string appointers = "";
+                string description = this.introductionTextBox.Text;
+                Book newBook = new Book(bookId, name, bookShelfId, author, description, lendTime, clientName, sort, appointers) { State = "可正常使用"};
+                if(path != null)
+                {
+                    File.Copy(path, resPath + @"\" + newBook.Name + ".jpg");
+                    newBook.imagePath = path;
+                }
+                currentShelf.AddBook(newBook);
+                BookShelfService.UpdateShelf(currentShelf);
+                MessageBox.Show("已添加新书！");
+                this.addButton.Enabled = false;
+                this.addButton.Visible = false;
+            }
+            else
+            {
+                MessageBox.Show("没有输入完整的信息！");
+            }
         }
 
-        private void finishButton_Click(object sender, EventArgs e)//完成
+       private void finishButton_Click(object sender, EventArgs e)//完成
         {
+            if(Flag == 2)
+            {
+                currentBook.Name = this.nameTextBox.Text;
+                currentBook.Author = this.authorTextBox.Text;
+                currentBook.Sort = this.sortTextBox.Text;
+                currentBook.Description = this.introductionTextBox.Text;
+                if(path != null)
+                {
+                    if (File.Exists(resPath + @"\" + currentBook.Name + ".jpg")) //判断文件是否存在
+                    {
+                        File.Delete(resPath + @"\" + currentBook.Name + ".jpg");
+                    }
+                    File.Copy(path, resPath + @"\" + currentBook.Name + ".jpg");
+                    currentBook.imagePath = path;
+                }
+                BookShelfService.UpdateBook(currentBook);
+            }
             this.Close();
+        }
+
+        public void findPath()
+        {
+            string basePath = Directory.GetCurrentDirectory();
+            resPath = basePath.Replace(@"\bin\Debug", @"\Resources");
+        }
+        private void chooseImageButton_Click(object sender, EventArgs e)//设置图像
+        {
+                this.openFileDialog.ShowDialog();
+                path = openFileDialog.FileName;
+                discoverPictureBox.Image = Image.FromFile(path);
+                MessageBox.Show(path);
         }
     }
 }
