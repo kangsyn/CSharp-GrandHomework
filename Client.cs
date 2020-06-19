ef35d249d1d42cb31a542d5de3cc19a6a876c4b5
TJW-0 <61374195+TJW-0@users.noreply.github.com>
2020-06-19 22:34:56 +0800

    Update Client.cs
    最终更新。

--- Client.cs
+++ Client.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,12 +8,10 @@ namespace Library
 {
     public class Client
     {
-        //预设管理员账号为Admin，密码为123
+        //预设管理员账号为管理员，密码为admin
         public string Id { get; set; }//用户编号
         public string Name { get; set; }//用户名
         public string Password { get; set; }//密码
-        public List<Book> LendBooks { get; set; }//被借阅的书籍
-        public List<Book> AppointedBooks { get; set; }//被预约的书籍
         public Client()
         {
             Id = Guid.NewGuid().ToString();
